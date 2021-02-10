1e90b04a50d1864bd498504e362d71a0ab638b57
KeziaSJ <kezia@deerom.com>
2021-02-10 15:56:22 +0530

    class.py

--- class.py
+++ class.py
@@ -1,10 +1,3 @@
-# class MyStuff(object):
-# 	def __init__(self):
-# 		self.tangerine = "And now a thousand years between"
-
-# 	def apple(self):
-# 		print("I AM CLASSY APPLES!")
-
 class MyClass:
 	x=5
 
