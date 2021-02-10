b39fecaa04f4b2e2698f70440e270c2681a805ef
KeziaSJ <kezia@deerom.com>
2021-02-10 13:49:51 +0530

    abc.py

---
+++ class.py
@@ -0,0 +1,12 @@
+# class MyStuff(object):
+# 	def __init__(self):
+# 		self.tangerine = "And now a thousand years between"
+
+# 	def apple(self):
+# 		print("I AM CLASSY APPLES!")
+
+class MyClass:
+	x=5
+
+p1=MyClass()
+print(p1.x)
